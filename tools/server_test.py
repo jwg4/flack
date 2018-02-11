a5a220e28a6f239f31a9da8f4460a1a895361fda
Jack Grahl <jack.grahl@gmail.com>
2018-02-11 01:16:01 +0000

    Run an actual server in the test process (only gunicorn).

---
+++ tools/server_test.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+import time
+
+
+def start_gunicorn()
+    args = ["gunicorn", "wsgi.app"]
+    p = subprocess.Popen(args=args, pwd="flack")
+    time.sleep(5)
+    p.kill()
+
+
+if __name__ == '__main__':
+    server_name = os.environ['SERVER']
+
+    if server_name == 'gunicorn':
+        start_gunicorn()
